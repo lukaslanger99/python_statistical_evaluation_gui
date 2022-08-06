43487967d185d270391a95314262d6dffc7db52b
Lukas Langer <lukas.langer99@gmx.de>
2022-08-07 01:16:49 +0200

    update chart + filter

--- main.py
+++ main.py
@@ -58,56 +58,80 @@ class App(tk.Frame):
         self.event_list.grid(row=1, column=0)
 
         def eventpicker_submit():
-            for i in self.event_list.curselection():
-                print(self.event_list.get(i))
+            self.print_chart(self.get_selected_events())
+            self.select_all_filter_checkbox_var.set(1)
+            for checkbox in self.filter_checkboxes:
+                checkbox["value"].set(1)
         tk.Button(self.parent, text="Select Events", command=eventpicker_submit).grid(row=1, column=1)
 
     def init_filter_list(self):
         filter_frame = tk.Frame(self.parent, bg="white")
         filter_frame.grid(row=2, column=0)
-        filter_checkboxes = []
+        self.filter_checkboxes = []
 
         def checkbox_select_all_clicked():
-            for checkbox in filter_checkboxes:
-                checkbox["value"].set(cb.get())
-        cb = tk.IntVar()
-        checkbox = tk.Checkbutton(filter_frame, text="Select All", variable=cb, command=checkbox_select_all_clicked)
-        checkbox.pack()
+            for checkbox in self.filter_checkboxes:
+                checkbox["value"].set(self.select_all_filter_checkbox_var.get())
+        self.select_all_filter_checkbox_var = tk.IntVar()
+        select_all_checkbox = tk.Checkbutton(filter_frame, text="Select All", variable=self.select_all_filter_checkbox_var, command=checkbox_select_all_clicked)
+        select_all_checkbox.pack()
         for label in self.labels:
             if label["type"] == "Integer":
                 checkbox_var = tk.IntVar()
                 checkbox = tk.Checkbutton(filter_frame, variable=checkbox_var, text=label["name"])
                 checkbox.pack()
-                filter_checkboxes.append({"checkbox": checkbox, "value": checkbox_var})
+                self.filter_checkboxes.append({"checkbox": checkbox, "value": checkbox_var, "checkbox_label_for": label["name"]})
         def submit_filters():
-            print("filters")
+            filter_selected_labels = []
+            for checkbox in self.filter_checkboxes:
+                if checkbox["value"].get():
+                    filter_selected_labels.append({"name": checkbox["checkbox_label_for"], "type": "Integer"})
+            if filter_selected_labels != []:
+                self.print_chart(self.get_selected_events(), filter_selected_labels)
         tk.Button(self.parent, text="Update", command=submit_filters).grid(row=3, column=0)
+
+    def get_selected_events(self):
+        all_items = self.event_list.get(0, tk.END) # tuple with text of all items in Listbox
+        sel_list = [all_items[item] for item in self.event_list.curselection()]
+        selected_events = []
+        for event_text in sel_list:
+            id = int(event_text[event_text.find("[")+1:event_text.find("]")])
+            for event in self.events:
+                if event["id"] == id:
+                    selected_events.append(event)
+        return selected_events
 
     def init_config_buttons(self):
         tk.Button(self.parent, text="New Event", command=self.listener_new_event_button_submit).grid(row=0, column=8)
         tk.Button(self.parent, text="Configure Labels", command=self.listener_config_button_submit).grid(row=1, column=8)
 
     def init_graph(self):
-        colors = []
-        for i in range(12):
-            colors.append('#%06X' % randint(0, 0xFFFFFF))
-        figure1 = Figure(figsize=(4,3), dpi=100)
-        subplot1 = figure1.add_subplot(111)
-        bar1 = FigureCanvasTkAgg(figure1, self.parent)
+        self.figure = Figure(figsize=(4,3), dpi=100)
+        self.subplot = self.figure.add_subplot(111)
+        bar1 = FigureCanvasTkAgg(self.figure, self.parent)
         bar1.name='latheesh'
         bar1.get_tk_widget().grid(row=3, column=1)
 
+    def print_chart(self, event_list_selection, filter_list_selection=None):
+        if event_list_selection == []:
+            return
+        if filter_list_selection is None:
+            filter_list_selection = self.labels
+        self.init_graph()
+        colors = []
+        for i in range(len(filter_list_selection)):
+            colors.append('#%06X' % randint(0, 0xFFFFFF))
         i = 0
-        for label in self.labels:
+        for label in filter_list_selection:
             if label["type"] == "Integer":
                 dates = []
                 values = []
-                for event in self.events:
+                for event in event_list_selection:
                     dates.append(event["date"])
                     values.append(event["event_labels"][label["name"]])
-                subplot1.plot(dates, values, color=colors[i+1], linestyle='dashed', linewidth = 1, marker='o', markerfacecolor=colors[i+1], markersize=5, label=label["name"])
+                self.subplot.plot(dates, values, color=colors[i], linestyle='dashed', linewidth = 1, marker='o', markerfacecolor=colors[i], markersize=5, label=label["name"])
                 i = i + 1
-        subplot1.legend()
+        self.subplot.legend()
 
     def listener_new_event_button_submit(self):
         new_event_window = tk.Toplevel(self.parent)
